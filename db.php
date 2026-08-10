c80f376a893df1301411ca82619617daf2107588
vs232352-hash <vs232352@gmail.com>
2026-08-10 17:30:03 +0530

    Create db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "blog";
+$dbuser = "root";
+$dbpass = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
